b7e82981159e875643237de0f37df05f16417c82
Meghna Bharadwaj <meghn.bharadwaj@gmail.com>
2021-06-26 22:45:27 +0530

    main api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb5440befec0556d9064cd5c1157e3a0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '09a05623-ab2e-40dd-b3ae-1345d598043a'
+};
